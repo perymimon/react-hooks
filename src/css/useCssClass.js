32b0aa69c7aab73b72294645802e4d229ff7a984
perym <pery.mimon@gmail.com>
2022-08-19 00:05:37 +0300

    useCssClass : add baseCalass

--- src/css/useCssClass.js
+++ src/css/useCssClass.js
@@ -1,12 +1,17 @@
-import {useDebugValue} from "react";
+import { useDebugValue } from "react";
 
 const ignoreList = ["undefined", "null", "", "0", "false", "NaN", "Infinity", "true"];
 
-export function useCssClass(classObject) {
+export function useCssClass(baseClasses, classObject) {
+    if (!classObject) return useCssClass('', baseClasses);
+ 
+    return baseClasses + " " + classes.join(" ");
+}
     let classes = Object.keys(classObject)
         .filter(key => !ignoreList.includes(key) && classObject[key])
         .join(" ");
 
+    classes += " " + baseClasses;
     useDebugValue(classes);
 
     return classes;
