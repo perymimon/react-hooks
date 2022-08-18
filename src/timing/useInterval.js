3524e0e1f460685d66f3b40422b3bd6d3269943b
perym <pery.mimon@gmail.com>
2022-08-18 23:24:57 +0300

    useInterval got {restart} action

--- src/timing/useInterval.js
+++ src/timing/useInterval.js
@@ -1,33 +1,32 @@
 import { useRef, useEffect, useCallback } from 'react';
-import useLatest from 'advence/useLatest';
+import useLatest from '../advence/useLatest.js';
 
 export function useInterval(callback, delay, options) {
     const immediate = options?.immediate;
+    const autoStart = options?.autoStart ?? true;
 
     const fnRef = useLatest(callback);
     const timerRef = useRef();
 
-    useEffect(() => {
+    const restart = useCallback(() => {
         if (Number(delay) != delay || delay <= 0) return;
-
         if (immediate) {
             fnRef.current();
         }
+        clearInterval(timerRef.current);
         timerRef.current = setInterval(() => {
             fnRef.current();
         }, delay);
-
-        return () => {
-            if (timerRef.current) {
-                clearInterval(timerRef.current);
-            }
-        };
-    }, [delay]);
+    },[]);
 
     const clear = useCallback(() => {
         clearInterval(timerRef.current);
     }, []);
 
-    return clear;
-}
+    useEffect(() => {
+        if(autoStart) restart();
+        return clear;
+    }, [delay]);
 
+    return {clear, restart};
+}
